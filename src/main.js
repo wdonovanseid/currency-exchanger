3d428938372bae84d2ccabe7f0a6269ffca82993
William Donovan-Seid <wdstwo@live.com>
2020-09-11 09:38:07 -0700

    add currencyFrom and put in switch statement for each output

--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ $(document).ready(function() {
   $("form#exchange").submit(function(event) {
     event.preventDefault();
     const dollar = parseInt($(`#amount`).val());
-    // const currency = $(`#currency`).val();
+    const currencyFrom = $(`#currency-from`).val();
+    const currencyTo = $(`#currency-to`).val();
 
     let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currencyFrom}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -23,7 +24,32 @@ $(document).ready(function() {
     request.send();
 
     function getElements(response) {
-      let output = dollar * response.conversion_rates.AUD;
+      let output;
+      switch (currencyTo) {
+        case "USD":
+          output = dollar * response.conversion_rates.USD;
+          break;
+        case "AUD":
+          output = dollar * response.conversion_rates.AUD;
+          break;
+        case "BGN":
+          output = dollar * response.conversion_rates.BGN;
+          break;
+        case "CAD":
+          output = dollar * response.conversion_rates.CAD;
+          break;
+        case "CHF":
+          output = dollar * response.conversion_rates.CHF;
+          break;
+        case "CNY":
+          output = dollar * response.conversion_rates.CNY;
+          break;
+        case "EGP":
+          output = dollar * response.conversion_rates.EGP;
+          break;
+        case "EUR":
+          output = dollar * response.conversion_rates.EUR;
+      }
       $("#output").text(`${output}`);
     }
   });
