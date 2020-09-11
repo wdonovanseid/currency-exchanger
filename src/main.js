ffb2dafa01e0766d34677cfca86ebc39ff275fcb
William Donovan-Seid <wdstwo@live.com>
2020-09-11 09:02:51 -0700

    add basic main.js setup

--- src/main.js
+++ src/main.js
@@ -4,5 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
+  attachListeners();
+  $("form#exchange").submit(function(event) {
+    event.preventDefault();
+    const dollar = parseInt($(`#amount`).val());
+    const asdf = $(`#currency`).val();
 
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${dollar}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $("#output").text(`${response.conversion_rates.asdf}`)
+    }
+  });
 });
