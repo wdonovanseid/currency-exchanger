9b149320b5c1cd4e1124d8f79da4162bd7ad71b7
William Donovan-Seid <wdstwo@live.com>
2020-09-11 13:40:07 -0700

    work on making a loop instead of switch in exchange-api.js

--- src/main.js
+++ src/main.js
@@ -31,14 +31,5 @@ $(document).ready(function() {
     }, function (error) {
       $(`#output`).text(`There was an error processing your request: ${error}`);
     });
-
-    let field = $().val();
-    if (sessionStorage.getItem("autosave")) {
-      field.value = sessionStorage.getItem("");
-    }
-
-    field.addEventListener("change", function() {
-      sessionStorage.setItem("autosave", field.value);
-    })
   });
 });
