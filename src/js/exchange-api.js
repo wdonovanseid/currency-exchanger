051e35369bd7046b7f8e54cc0fe30447cfa0919d
William Donovan-Seid <wdstwo@live.com>
2020-09-11 10:21:41 -0700

    add complete promise to main.js, move getExchangeRate to exchange-api.js

--- src/js/exchange-api.js
+++ src/js/exchange-api.js
@@ -8,9 +8,39 @@ export class ExchangeRate {
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
   }
+}
+
+export function getConversionRates(response,currencyTo,dollars) {
+  let output;
+  switch (currencyTo) {
+  case "USD":
+    output = dollars * response.conversion_rates.USD;
+    break;
+  case "AUD":
+    output = dollars * response.conversion_rates.AUD;
+    break;
+  case "BGN":
+    output = dollars * response.conversion_rates.BGN;
+    break;
+  case "CAD":
+    output = dollars * response.conversion_rates.CAD;
+    break;
+  case "CHF":
+    output = dollars * response.conversion_rates.CHF;
+    break;
+  case "CNY":
+    output = dollars * response.conversion_rates.CNY;
+    break;
+  case "EGP":
+    output = dollars * response.conversion_rates.EGP;
+    break;
+  case "EUR":
+    output = dollars * response.conversion_rates.EUR;
+  }
+  return output;
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import {ExchangeRate} from './js/exchange-api.js';
+import {getConversionRates} from './js/exchange-api.js';
 
 function clearFields() {
   $(`#amount`).val("");
@@ -18,56 +19,13 @@ $(document).ready(function() {
     const currencyTo = $(`#currency-to`).val();
     clearFields();
 
-    let promise = ExchangeRate.getExchangeRate(currencyFrom)
+    let promise = ExchangeRate.getExchangeRate(currencyFrom);
     promise.then(function(response) {
-      const body = JSON.parse(response);
-      
+      const exchangeAPI = JSON.parse(response);
+      const output = getConversionRates(exchangeAPI,currencyTo,dollars);
+      $("#output").text(`${dollars} ${currencyFrom} is ${output} ${currencyTo}`);
     }, function (error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
     });
-
-    let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currencyFrom}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getConversionRates(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getConversionRates(response) {
-      console.log(response.conversion_rates.ASDFDSD)
-      let output;
-      switch (currencyTo) {
-        case "USD":
-          output = dollars * response.conversion_rates.USD;
-          break;
-        case "AUD":
-          output = dollars * response.conversion_rates.AUD;
-          break;
-        case "BGN":
-          output = dollars * response.conversion_rates.BGN;
-          break;
-        case "CAD":
-          output = dollars * response.conversion_rates.CAD;
-          break;
-        case "CHF":
-          output = dollars * response.conversion_rates.CHF;
-          break;
-        case "CNY":
-          output = dollars * response.conversion_rates.CNY;
-          break;
-        case "EGP":
-          output = dollars * response.conversion_rates.EGP;
-          break;
-        case "EUR":
-          output = dollars * response.conversion_rates.EUR;
-      }
-      $("#output").text(`${dollars} ${currencyFrom} is ${output} ${currencyTo}`);
-    }
   });
 });
